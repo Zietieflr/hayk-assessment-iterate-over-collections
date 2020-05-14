265547f9e233b540a9b7b7e4d9bc9b9178534fa4
Zietieflr <lrmcguire93@gmail.com>
2020-05-14 16:43:51 -0600

    Further cleanup and syntax update

--- app.rb
+++ app.rb
@@ -1,59 +1,56 @@
 require 'pry'
 
 class Company
-    attr_accessor :name, :size
+  attr_accessor :name, :size
 
-    def initialize(name, size)
-        @name = name
-        @size = size
-    end
+  def initialize(name, size)
+    @name = name
+    @size = size
+  end
 end
 
 companies = [
-    Company.new('Alpha', 30),
-    Company.new('Beta', 300),
-    Company.new('Delta', 3000)
+  Company.new('Alpha', 30),
+  Company.new('Beta', 300),
+  Company.new('Delta', 3000),
 ]
 
 ### Your code below this
 
 #As we were to code below the above line, the following were not made into methods due to scope. 
-#puts left in for testing purposes and understanding output. 
-#empty comments for readability between tests, as they would normally be methods. 
+#puts left in for testing purposes and understanding output.  
 
-#
+####
 puts companies.map { |company| 
-    "#{company.name} - #{company.size}"    
+  "#{company.name} - #{company.size}"
 }
 
-#
+####
 puts companies.reduce(0) { |sum, company|
-    sum + company.size 
+  sum + company.size 
 }
 
-#
+####
 large_companies = companies.select { |company|
-    company.size > 100 
+  company.size > 100 
 }
 puts large_companies.map { |company|
-    company.name 
+  company.name
 }
 
-#
+####
 beta_company = companies.find { |company| 
-    company.name === "Beta"
+  company.name == 'Beta'
 }
-puts beta_company.name 
+puts beta_company.name
 
-#
-biggest_company = companies.reduce { |biggest, company|
-    biggest.size > company.size ? biggest : company; 
+####
+biggest_company = companies.reduce { |biggest_company, company|
+  biggest_company.size > company.size ? biggest_company : company
 }
-puts biggest_company.name; 
+puts biggest_company.name;
 
-#
-sorted_companies = companies.sort_by { |company| 
-    company.size
-}
+####
+sorted_companies = companies.sort_by(&:size)
 sorted_companies = sorted_companies.reverse_each
 puts sorted_companies.map(&:name)
